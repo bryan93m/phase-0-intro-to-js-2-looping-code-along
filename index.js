d46ce899ee66e0715b6e22ddfbec2b3b20dbb79f
Bryan Meadows <bryan1993meadows@gmail.com>
2022-08-11 11:24:28 -0600

    commit tree ca2fcb0998e82d03b8bd41857153e839e233c56d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, ename){
+    let newArray = [];
+    for(let i = 0; i < name.length; i++){
+        newArray.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return newArray
+}
+
+function countDown(number){
+    let count = number;
+    while(count >= 0){
+        console.log(count--)
+    }
+}
